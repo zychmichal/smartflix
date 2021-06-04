be619e7ed6f27baf980d7fa13e02950fd3f9aa8a
Michal Zych <michal.zych@smartpension.co.uk>
2021-06-04 13:45:58 +0200

    Add simple worker to communicate with omdbapi and create movie, add rails credentials configuration and simple Movie validation

--- spec/workers/create_movie_if_exist_worker_spec.rb
+++ spec/workers/create_movie_if_exist_worker_spec.rb
@@ -1,4 +1,4 @@
 require 'rails_helper'
-RSpec.describe CreateMovieWorker, type: :worker do
+RSpec.describe CreateMovieIfExistWorker, type: :worker do
   pending "add some examples to (or delete) #{__FILE__}"
 end
