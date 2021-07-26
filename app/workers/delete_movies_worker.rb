3739356d0e8d9e2b1e760bc42022fda8500824d6
Michal Zych <michal.zych@smartpension.co.uk>
2021-07-26 13:25:00 +0200

    Add delete movies worker and scheduling for new workers

--- app/workers/delete_movies_worker.rb
+++ app/workers/delete_movies_worker.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
 class DeleteMoviesWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: 'movies', retry: false
+
+  def perform
+    Movie.all.each { |movie| movie.delete if not_updated_since_two_days?(movie) }
+  end
+
+  private
+
+  def not_updated_since_two_days?(movie)
+    ((Time.zone.now - movie.updated_at) / 1.day) > 2
+  end
 end
