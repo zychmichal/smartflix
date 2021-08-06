ba9fe6fd1a3739aa7bcbc368c08b3962294f4733
Michal Zych <michal.zych@smartpension.co.uk>
2021-08-06 07:56:38 +0200

    Temporary solution for bug fixing with updated_at field

--- app/services/update_movie_from_api_service.rb
+++ app/services/update_movie_from_api_service.rb
@@ -5,8 +5,11 @@ class UpdateMovieFromApiService
     @movie_adapter = movie_adapter
   end
 
+  # FIXME: try find another way to update movie (for example new field in movie record)
   def update_movie(movie)
     updated_movie_attributes = @movie_adapter.find_by(title: movie.title, year: movie.year)
-    movie.update!(updated_movie_attributes.to_h)
+    return if updated_movie_attributes.nil?
+
+    movie.touch if movie.update!(updated_movie_attributes.to_h)
   end
 end
